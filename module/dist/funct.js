1d3f5a2fc865b9b1a033a0667076bd3347543662
Rakibhsaniu <rakibhsanoyoniu@gmail.com>
2023-11-01 23:12:51 +0600

    function

---
+++ module/dist/funct.js
@@ -0,0 +1,6 @@
+"use strict";
+const nam = (name1, name2) => {
+    return `${name1} ${name2}`;
+};
+nam('rakib', 'hasan');
+console.log(nam);
---
+++ module/src/funct.ts
@@ -0,0 +1,6 @@
+const nam = (name1:string,name2:string):string => {
+    return `${name1} ${name2}`;
+}
+
+nam('rakib','hasan')
+console.log(nam)
